afddc880a0d49a116cec05fcfadd8f955380df9b
Alema <stone.alema@gmail.com>
2025-05-20 14:47:33 -1000

    Practice Variable Types

--- LearnHTML2/index.js
+++ LearnHTML2/index.js
@@ -1,3 +1,10 @@
 
-let name = 'Alema';
-console.log(name);
+const interestRate = 0.3;
+//interestRate = 1;
+console.log(interestRate);
+
+let name = "Mateo"; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let lastName = null; // clear the variable
