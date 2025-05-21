711889ba5ff1ac56e4937eec12cce1b8f36d8841
Alema <stone.alema@gmail.com>
2025-05-20 22:36:08 -1000

    Add Input form

---
+++ LearnHTML2/tutorial2/to-do-list/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+  <>
+    <form className="new-item-form">
+      <div classname="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item"/>
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+  </>
+  )
+}
