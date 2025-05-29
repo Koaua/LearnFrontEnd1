055ab4cfe3f3683a585271e9052c8557e6f3cd64
Alema <125333005+Koaua@users.noreply.github.com>
2025-05-28 15:12:42 -1000

    Add main function

--- LearnFrontEnd3/learnfrontend3/src/App.js
+++ LearnFrontEnd3/learnfrontend3/src/App.js
@@ -1,2 +1,36 @@
 import "./App.css";
 import { React, useState } from "react";
+
+function App() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [contact, setContact] = useState("");
+    const [gender, setGender] = useState("male");
+    const [subjects, setSubjects] = useState({
+        english: true,
+        maths: false,
+        physics: false,
+    });
+    const [resume, setResume] = useState("");
+    const [url, setUrl] = useState();
+    const [selectedOption, setSelectedOption] =
+        useState("");
+    const [about, setAbout] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(
+            firstName,
+            lastName,
+            email,
+            contact,
+            gender,
+            selectedOption,
+            subjects,
+            resume,
+            url,
+            about
+        );
+        
+    };
