8bb7b2c02d4b03c2a8a5b38482725cec30227d74
Alema <125333005+Koaua@users.noreply.github.com>
2025-05-28 15:10:50 -1000

    Add imports

--- LearnFrontEnd3/learnfrontend3/src/App.js
+++ LearnFrontEnd3/learnfrontend3/src/App.js
@@ -1,19 +1,2 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">
-        </header>
-      </div>
-      <div>
-        <p>Adding second div</p>
-      </div>
-    </>
-
-  );
-}
-
-export default App;
+import "./App.css";
+import { React, useState } from "react";
