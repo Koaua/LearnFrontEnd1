460d3f063d1766f7bdae8ae6bd77d35cf153c4d1
Alema <125333005+Koaua@users.noreply.github.com>
2025-06-03 09:43:39 -1000

    Change html content

--- LearnFrontEnd4/tutorial1/src/App.js
+++ LearnFrontEnd4/tutorial1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I'm changing the html content!
         </a>
       </header>
     </div>
