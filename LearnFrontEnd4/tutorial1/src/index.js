ed107c1bb3d6f66c9f4cec3cb1f0895585f74a8f
Alema <125333005+Koaua@users.noreply.github.com>
2025-06-03 14:58:28 -1000

    Make first component

--- LearnFrontEnd4/tutorial1/src/index.js
+++ LearnFrontEnd4/tutorial1/src/index.js
@@ -1,31 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
 const myFirstElement = (
-  <table className="myTable">
-    <tr>
-      <th>Player</th>
-      <th>Team</th>
-    </tr>
-    <tr>
-      <td>J Burrow</td>
-      <td>QB</td>
-    </tr>
-    <tr>
-      <td>D Henry</td>
-      <td>RB</td>
-    </tr>
-    <tr>
-      <td>A Kamara</td>
-      <td>RB</td>
-    </tr>
-    <tr>
-      <td>M Nabers</td>
-      <td>WR</td>
-    </tr>
-  </table>
+  <>
+    <table className="myTable">
+      <tr>
+        <th>Player</th>
+        <th>Team</th>
+      </tr>
+      <tr>
+        <td>J Burrow</td>
+        <td>QB</td>
+      </tr>
+      <tr>
+        <td>D Henry</td>
+        <td>RB</td>
+      </tr>
+      <tr>
+        <td>A Kamara</td>
+        <td>RB</td>
+      </tr>
+      <tr>
+        <td>M Nabers</td>
+        <td>WR</td>
+      </tr>
+    </table>
+  </>
 )
+
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<Car/>);
 
 
