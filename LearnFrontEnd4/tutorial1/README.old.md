5e4e8f81eec09625a1da90cee2af2dea1f52183c
Alema <125333005+Koaua@users.noreply.github.com>
2025-06-03 14:42:00 -1000

    Add table using jsx

--- LearnFrontEnd4/tutorial1/README.old.md
+++ LearnFrontEnd4/tutorial1/README.old.md
@@ -1,1 +1,3 @@
 #W3 Schools React Tutorial
+
+https://www.w3schools.com/REACT/react_es6_variables.asp
--- LearnFrontEnd4/tutorial1/src/index.js
+++ LearnFrontEnd4/tutorial1/src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>
-
+const myFirstElement = (
+  <table className="myTable">
+    <tr>
+      <th>Player</th>
+      <th>Team</th>
+    </tr>
+    <tr>
+      <td>J Burrow</td>
+      <td>QB</td>
+    </tr>
+    <tr>
+      <td>D Henry</td>
+      <td>RB</td>
+    </tr>
+    <tr>
+      <td>A Kamara</td>
+      <td>RB</td>
+    </tr>
+    <tr>
+      <td>M Nabers</td>
+      <td>WR</td>
+    </tr>
+  </table>
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
+
+
