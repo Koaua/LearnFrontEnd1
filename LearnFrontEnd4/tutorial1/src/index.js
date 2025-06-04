6024763f278f9db50addf5cf7d49e3d8e46fcbe6
Alema <125333005+Koaua@users.noreply.github.com>
2025-06-03 15:03:06 -1000

    First Prop!

--- LearnFrontEnd4/tutorial1/src/index.js
+++ LearnFrontEnd4/tutorial1/src/index.js
@@ -29,13 +29,17 @@ const myFirstElement = (
   </>
 )
 
-class Car extends React.Component {
+/* class Car extends React.Component {
   render() {
     return <h2>Hi, I am a Car!</h2>;
   }
+}*/
+
+function Car(props) {
+  return <h2>I am a {props.color} Car!</h2>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car/>);
+root.render(<Car color="red"/>);
 
 
